1a45ffc3098ad2f38f5753b9021e8f13e1313562
notPhani <phaniexplains14@gmail.com>
2025-07-02 22:40:23 +0530

    initialize main.go file for server setup

---
+++ Backend/main.go
@@ -0,0 +1,11 @@
+//This is responsible for starting the server and setting up the routes
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is responsible for server connections")
+}
